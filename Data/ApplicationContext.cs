2b3c6cfddfa9f1bb6d727c75512818c50b48ef61
Hakan-Baglan <baglanhakan42@gmail.com>
2023-09-28 11:26:38 +0300

    first commit

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using bookDemo.Models;
+
+namespace bookDemo.Data
+{
+    public static class ApplicationContext
+    {
+        public static List<Book> Books { get; set; }
+        static ApplicationContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book() {Id=1, Title="Kargöz ve Hacivat", Price=75 },
+                new Book() {Id=2, Title="Kel Oğlan", Price=145 },
+                new Book() {Id=3, Title="7. Koğuştaki Mucize", Price=124 },
+            };
+        }
+    }
+}
